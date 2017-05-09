1b5c19923b1759f37b0ec94ab027fc1aa7608799
Andrii Kovalchuk <tieorange@gmail.com>
2017-05-09 21:43:39 +0200

    sends notification

--- src/main/kotlin/ContainerRunner.kt
+++ src/main/kotlin/ContainerRunner.kt
@@ -3,10 +3,12 @@ import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseCredentials
 import de.bytefish.fcmjava.client.FcmClient
 import de.bytefish.fcmjava.client.settings.PropertiesBasedSettings
+import de.bytefish.fcmjava.model.enums.PriorityEnum
 import de.bytefish.fcmjava.model.options.FcmMessageOptions
 import de.bytefish.fcmjava.requests.data.DataUnicastMessage
 import java.io.FileInputStream
-import java.nio.file.Path
+import java.nio.charset.Charset
+import java.nio.file.FileSystems
 import java.time.Duration
 
 /**
@@ -14,20 +16,27 @@ import java.time.Duration
  */
 
 fun main(args: Array<String>) {
-    ServiceRunner().run()
-
     initFirebase()
     initFCM()
+
+    ServiceRunner().run()
+
 }
 
 fun initFCM() {
-    val client = FcmClient(PropertiesBasedSettings.createFromDefault())
+    val path = FileSystems.getDefault().getPath(".fcmjava/fcmjava.properties");
+    val client = FcmClient(PropertiesBasedSettings.createFromFile(path, Charset.defaultCharset()))
 
     val options = FcmMessageOptions.builder()
-            .setTimeToLive(Duration.ofHours(1))
+            .setTimeToLive(Duration.ofDays(21))
+            .setPriorityEnum(PriorityEnum.High)
             .build()
+    val data = OrderPush("sdfdsfsdf", "userId", "TRF3", "sdflsdfkjsdlfj")
 
-    client.send(DataUnicastMessage(options, "", ))
+    val to = "c_l8mzJ-DcQ:APA91bGRmOaPJQC1xS85DAGYG63AiAQA2R1VxXQBJFzxd9E_29seXXTaIpCEGPFqixroK16mY4zmB7-MQFW2Sl0HV5i64K4T-P3NjAgozru2LqAsIXjzoGpaFcvjyoHQExGeFCedl5OH"
+
+    client.send(DataUnicastMessage(options, to, data))
+    println("Notification sent")
 }
 
 private fun initFirebase() {
---
+++ src/main/kotlin/OrderPush.kt
@@ -0,0 +1,5 @@
+class OrderPush(val token: String,
+                val userUID: String,
+                val secretCode: String,
+                val orderKey: String) {
+}
